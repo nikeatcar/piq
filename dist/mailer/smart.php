c3c0c4fd94bfaaea8582bde80a0614e16fc67fb3
nikeatcar <102768128+nikeatcar@users.noreply.github.com>
2022-04-26 20:15:20 +0300

    Update smart.php

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -14,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ssrnikita@gmail.com';                 // Наш логин
-$mail->Password = 'cwmdnvsspbnlkbcj';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('ssrnikita@gmail.com', 'piq');   // От кого письмо 
-$mail->addAddress('beyaxe5797@viemery.com');     // Add a recipient
+$mail->setFrom('', 'piq');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
