12ea959eee0b99fa9f865bbe4e2d499bfa9b3eb6
nikeatcar <ssrnikita@gmail.com>
2022-04-29 16:05:58 +0400

    Animations and mailer

--- src/js/script.js
+++ src/js/script.js
@@ -61,30 +61,35 @@ function validateForms(form){
 validateForms('#cnsl-form');
 validateForms('#footer-form');
 validateForms('#popup-form');
+$('input[name=phone]').mask("+375 (99) 999 99 99");
 
 /* ОТПРАВКА ПОЧТЫ */
-$('form').submit(function(e){
-    e.preventDefault();
-
-    if (!$(this).valid()){
-        return;
+// Отправка данных на сервер
+function send(event, php){
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response); // Ебанный internet explorer 11
+            console.log(json);
+            
+            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+            if (json.result == "success") {
+                // Если сообщение отправлено
+                alert("Сообщение отправлено");
+            } else {
+                // Если произошла ошибка
+                alert("Ошибка. Сообщение не отправлено");
+            }
+        // Если не удалось связаться с php файлом
+        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+    
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() {alert("Ошибка отправки запроса");};
+    req.send(new FormData(event.target));
     }
-
-    $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-    }).done(function(){
-        $(this).find("input").val("");
-        $('#cnsl, #order').fadeOut();
-        $('.overlay, #tnx').fadeIn('slow');
-
-        $('form').trigger('reset');
-    });
-    return false;
-});
-
-$('input[name=phone]').mask("+375 (99) 999 99 99");
 
     /* МЯГКИЙ СКРОЛЛ И PGUP */
     $(window).scroll(function(){
---
+++ src/phpmailer/Exception.php
@@ -0,0 +1,7 @@
+<?php
+namespace PHPMailer\PHPMailer;
+class Exception extends\Exception{
+    public function errorMessage(){
+        return'<strong>'.htmlspecialchars($this->getMessage(),ENT_COMPAT|ENT_HTML401)."</strong><br />\n";
+    }
+}
